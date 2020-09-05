6c96c4ce369e0f5ce1845a7f7219d04c77c8477b
qinhouping <877795526@qq.com>
2020-09-05 17:36:56 +0800

    东财通v2

--- EMChat2/ApplicationBooter.cs
+++ EMChat2/ApplicationBooter.cs
@@ -5,18 +5,34 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Configuration;
+using System.Threading;
+using System.Windows;
 
 namespace EMChat2
 {
     public class ApplicationBooter : ApplicationBase
     {
+        private bool allowMulitInstance = Convert.ToBoolean(ConfigurationManager.AppSettings["AllowMulitInstance"]);
         public static ApplicationBooter Current { get; protected set; }
 
         public ApplicationBooter() : base()
         {
             if (Current != null) throw new InvalidProgramException("An instantiation of the applicationbooter object already exists");
             Current = this;
-            this.OnStartUp<LoginViewModel>();
+            bool isNewInstance = false;
+            string appName = System.Reflection.Assembly.GetExecutingAssembly().GetName().Name;
+            Mutex mutex = new Mutex(true, appName, out isNewInstance);
+
+            if (!isNewInstance)
+            {
+                MessageBox.Show("The app is running now");
+                Application.Current.Shutdown();
+            }
+            else
+            {
+                this.OnStartUp<LoginViewModel>();
+            }
         }
     }
 }
